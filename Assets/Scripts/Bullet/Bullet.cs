f0643b177911408fb0d24e5ab504e9d21a07613a
Kellen-Walker <kellen.walker@mergevr.com>
2017-03-03 18:25:17 -0600

    Project Files
    Actual init commit with project files

---
+++ Assets/Scripts/Bullet/Bullet.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bullet : MonoBehaviour {
+
+	void OnCollisionEnter(Collision collision)
+	{
+		GameObject hit = collision.gameObject;
+		PlayerHealth health = hit.GetComponent<PlayerHealth>();
+		if (health  != null)
+		{
+			health.TakeDamage(10);
+		}
+
+		Destroy(gameObject);
+	}
+}
